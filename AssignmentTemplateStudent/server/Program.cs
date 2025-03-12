cc17d7d6ea8a68b9545351b4483c5fc407a6ade6
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2025-03-12 14:26:23 +0100

    Implement message handling and welcome response in UDP server

--- AssignmentTemplateStudent/server/Program.cs
+++ AssignmentTemplateStudent/server/Program.cs
@@ -39,7 +39,7 @@ class ServerUDP
     static string recordsContent = File.ReadAllText(recordsFile);
     static List<DNSRecord>? records = JsonSerializer.Deserialize<List<DNSRecord>>(recordsContent);
 
-    
+
     public static void start()
     {
         // TODO: [Create a socket and endpoints and bind it to the server IP address and port number]
@@ -48,25 +48,46 @@ class ServerUDP
             throw new InvalidOperationException("Invalid settings in configuration file.");
         }
 
-        IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(setting.ServerIPAddress),setting.ServerPortNumber);
-        IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Parse(setting.ClientIPAddress),setting.ClientPortNumber);
-        
+        IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(setting.ServerIPAddress), setting.ServerPortNumber);
+        IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Parse(setting.ClientIPAddress), setting.ClientPortNumber);
+
         Socket listener = new(serverEndpoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
         listener.Bind(serverEndpoint);
 
         // TODO:[Receive and print a received Message from the client]
         byte[] buffer = new byte[1024];
         int bytesReceived = listener.Receive(buffer);
-        string message = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
-        Console.WriteLine(message);
-
+        string messageJson = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+        Console.WriteLine("Received message: " + messageJson);
 
         // TODO:[Receive and print Hello]
-
-
+        Message? receivedMessage = JsonSerializer.Deserialize<Message>(messageJson);
+        if (receivedMessage != null && receivedMessage.MsgType == MessageType.Hello)
+        {
+            Console.WriteLine("Received Hello message from client.");
+        }
 
         // TODO:[Send Welcome to the client]
+        if (receivedMessage != null && receivedMessage.MsgType == MessageType.Hello)
+        {
+            Console.WriteLine("Received Hello message from client.");
 
+            // Create a Welcome message
+            Message welcomeMessage = new()
+            {
+                MsgId = receivedMessage.MsgId + 1,
+                MsgType = MessageType.Welcome,
+                Content = "Welcome to the server!"
+            };
+
+            // Serialize the Welcome message to JSON
+            string welcomeMessageJson = JsonSerializer.Serialize(welcomeMessage);
+            byte[] welcomeMessageBytes = Encoding.ASCII.GetBytes(welcomeMessageJson);
+
+            // Send the Welcome message to the client
+            listener.SendTo(welcomeMessageBytes, clientEndpoint);
+            Console.WriteLine("Sent Welcome message to client.");
+        }
 
         // TODO:[Receive and print DNSLookup]
 
